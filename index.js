e8e74196ec4968989e5904343b79a408fb048ce6
mabdi <m.abdi@aut.ac.ir>
2021-04-09 10:06:52 +0200

    v0 release

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
+
+const wait = require('./smallamp');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    const testclass = core.getInput('testclass');
+    core.info(`Test Amplification started: ${testclass}`);
+    await smallamp(testclass);
+    core.info(`Test Amplification finished: ${testclass}`);
+    core.setOutput('time', 'todo');
   } catch (error) {
     core.setFailed(error.message);
   }
