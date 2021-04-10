983e980836a3f5534cded50cb3a3a0981c2878d5
mabdi <m.abdi@aut.ac.ir>
2021-04-10 09:49:21 +0200

    add zip folder to env

--- dist/index.js
+++ dist/index.js
@@ -13,11 +13,14 @@ const os = __nccwpck_require__(87)
 
 
 const SMALLAMP_CI_HOME = path.join(os.homedir(), '.smallAmpCI')
+const SMALLAMP_CI_ZIPS = path.join(SMALLAMP_CI_HOME, '_zip')
 const SMALLAMP_DOWNLOAD = 'https://github.com/mabdi/SmallAmp-runner/archive/master.tar.gz'
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
+     await io.mkdirP(SMALLAMP_CI_ZIPS);
+     core.exportVariable('SMALLAMP_CI_ZIPS', SMALLAMP_CI_ZIPS);
      let tempDir = path.join(os.homedir(), '.smallAmpCI-temp')
      const toolPath = await tc.downloadTool(SMALLAMP_DOWNLOAD)
      tempDir = await tc.extractTar(toolPath, tempDir)
--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const os = require('os')
 
 
 const SMALLAMP_CI_HOME = path.join(os.homedir(), '.smallAmpCI')
+const SMALLAMP_CI_ZIPS = path.join(SMALLAMP_CI_HOME, '_zip')
 const SMALLAMP_DOWNLOAD = 'https://github.com/mabdi/SmallAmp-runner/archive/master.tar.gz'
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
+     await io.mkdirP(SMALLAMP_CI_ZIPS);
+     core.exportVariable('SMALLAMP_CI_ZIPS', SMALLAMP_CI_ZIPS);
      let tempDir = path.join(os.homedir(), '.smallAmpCI-temp')
      const toolPath = await tc.downloadTool(SMALLAMP_DOWNLOAD)
      tempDir = await tc.extractTar(toolPath, tempDir)
