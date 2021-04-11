efac5080fdce2e276c09c08276d2be1b06d76ee4
mabdi <m.abdi@aut.ac.ir>
2021-04-11 07:25:35 +0200

    add repo name to artifact name

--- post.js
+++ post.js
@@ -12,8 +12,9 @@ async function run() {
   try {
      const dir = process.env.SMALLAMP_CI_ZIPS
      const runId = process.env.GITHUB_RUN_NUMBER
+     const reponame = process.env.reponame
      const artifactClient = artifact.create()
-     const artifactName = 'smallAmp-output-run' + runId;
+     const artifactName = 'smallAmp-'+ reponame +'-run' + runId;
      const files = fs.readdirSync(dir).filter(fn => fn.endsWith('.zip')).map(x => dir + '/' + x);
      if (files.length > 0)
      {
