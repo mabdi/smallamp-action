152bca326a7cde9c7fa9c1200ae15583cf06b5b2
mabdi <m.abdi@aut.ac.ir>
2021-04-19 09:47:46 +0200

    result and logs artifcats

--- dist2/index.js
+++ dist2/index.js
@@ -7238,19 +7238,35 @@ async function run() {
      const dir = process.env.SMALLAMP_CI_ZIPS
      const runId = process.env.GITHUB_RUN_NUMBER
      const reponame = process.env.reponame
+
      const artifactClient = artifact.create()
-     const artifactName = 'smallAmp-'+ reponame +'-run' + runId;
-     const files = fs.readdirSync(dir).filter(fn => fn.endsWith('.zip')).map(x => dir + '/' + x);
-     if (files.length > 0)
+
+     const artifactResults = 'smallAmp-results-'+ reponame +'-run' + runId;
+     const artifactLogs = 'smallAmp-logs-'+ reponame +'-run' + runId;
+     const files_results = fs.readdirSync(dir).filter(fn => fn.endsWith('results.zip')).map(x => dir + '/' + x);
+     const files_logs = fs.readdirSync(dir).filter(fn => fn.endsWith('logs.zip')).map(x => dir + '/' + x);
+     if (files_results.length > 0)
      {
          const rootDirectory = dir // Also possible to use __dirname
          const options = {
             continueOnError: false
          }
-         const uploadResponse = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options)
+         const uploadResponse = await artifactClient.uploadArtifact(artifactResults, files_results, rootDirectory, options)
      }else{
-         core.info('No files to build the artifact. ')
+         core.info('No result files to build the artifact. ')
      }
+
+     if (files_logs.length > 0)
+     {
+         const rootDirectory = dir // Also possible to use __dirname
+         const options = {
+            continueOnError: false
+         }
+         const uploadResponse = await artifactClient.uploadArtifact(artifactLogs, files_logs, rootDirectory, options)
+     }else{
+         core.info('No logs files to build the artifact. ')
+     }
+
   } catch (error) {
     core.setFailed(error.message);
   }
