1e91eb12350732fd8bb938acc2772940b664faee
mabdi <m.abdi@aut.ac.ir>
2021-05-20 16:16:09 +0200

    remove overview uploading

--- dist2/index.js
+++ dist2/index.js
@@ -8417,39 +8417,36 @@ const path = __nccwpck_require__(5622)
 const os = __nccwpck_require__(2087)
 const fs = __nccwpck_require__(5747)
 
-async function build_overview() {
-    try {
-        let base = process.env.SMALLTALK_CI_IMAGE
-        base = base.substring(0, base.lastIndexOf("/") + 1);
-        const dir = process.env.SMALLAMP_CI_ZIPS
-        const job_id = process.env.SMALLAMP_PORTION
-        let myOutput = '';
-        let myError = '';
-        let detail_amp = '';
-        let sum_amp = '';
-        const options = {};
-        options.listeners = {
-          stdout: (data) => {
-            myOutput += data.toString();
-          },
-          stderr: (data) => {
-            myError += data.toString();
-          }
-        };
-        options.cwd = process.env.SMALLAMP_CI_HOME;
+// async function build_overview() {
+//     try {
+//         let base = process.env.SMALLTALK_CI_IMAGE
+//         base = base.substring(0, base.lastIndexOf("/") + 1);
+//         const dir = process.env.SMALLAMP_CI_ZIPS
+//         const job_id = process.env.SMALLAMP_PORTION
+//         let myOutput = '';
+//         let myError = '';
+//         let detail_amp = '';
+//         let sum_amp = '';
+//         const options = {};
+//         options.listeners = {
+//           stdout: (data) => {
+//             myOutput += data.toString();
+//           },
+//           stderr: (data) => {
+//             myError += data.toString();
+//           }
+//         };
+//         options.cwd = process.env.SMALLAMP_CI_HOME;
 
-        await exec.exec('python3', ['runner.py', '-r', 'amp', '-d', base, '-p', process.env.reponame], options);
-        detail_amp = myOutput;
-        await exec.exec('python3', ['runner.py', '-r', 'sum', '-d', base, '-p', process.env.reponame], options);
-        sum_amp = myOutput;
-
-        fs.writeFileSync(`${dir}/amp-${job_id}.txt`, detail_amp, function(error){ core.error(error) })
-        fs.writeFileSync(`${dir}/sum-${job_id}.txt`, sum_amp, function(error){ core.error(error) }) 
-
-    } catch (error) {
-    core.setFailed(error.message);
-  }
-}
+//         await exec.exec('python3', ['runner.py', '-r', 'amp', '-d', base, '-p', process.env.reponame], options);
+//         fs.writeFileSync(`${dir}/amp-${job_id}.txt`, myOutput, function(error){ core.error(error) });
+//         let myOutput = '';
+//         await exec.exec('python3', ['runner.py', '-r', 'sum', '-d', base, '-p', process.env.reponame], options);
+//         fs.writeFileSync(`${dir}/sum-${job_id}.txt`, myOutput, function(error){ core.error(error) }) 
+//     } catch (error) {
+//     core.setFailed(error.message);
+//   }
+// }
 
 async function build_artifacts() {
   try {
@@ -8461,10 +8458,10 @@ async function build_artifacts() {
 
      const artifactResults = 'smallAmp-results-'+ reponame +'-run' + runId;
      const artifactLogs = 'smallAmp-logs-'+ reponame +'-run' + runId;
-     const artifactOverview = 'smallAmp-overview-'+ reponame +'-run' + runId;
+     // const artifactOverview = 'smallAmp-overview-'+ reponame +'-run' + runId;
      const files_results = fs.readdirSync(dir).filter(fn => fn.endsWith('results.zip')).map(x => dir + '/' + x);
      const files_logs = fs.readdirSync(dir).filter(fn => fn.endsWith('logs.zip')).map(x => dir + '/' + x);
-     const files_overview = fs.readdirSync(dir).filter(fn => fn.endsWith('.txt')).map(x => dir + '/' + x);
+     // const files_overview = fs.readdirSync(dir).filter(fn => fn.endsWith('.txt')).map(x => dir + '/' + x);
      if (files_results.length > 0)
      {
          const rootDirectory = dir // Also possible to use __dirname
@@ -8487,23 +8484,23 @@ async function build_artifacts() {
          core.info('No logs files to build the artifact. ')
      }
 
-     if (files_overview.length > 0)
-     {
-         const rootDirectory = dir // Also possible to use __dirname
-         const options = {
-            continueOnError: false
-         }
-         const uploadResponse = await artifactClient.uploadArtifact(artifactOverview, files_overview, rootDirectory, options)
-     }else{
-         core.info('No overview files to build the artifact. ')
-     }
+     // if (files_overview.length > 0)
+     // {
+     //     const rootDirectory = dir // Also possible to use __dirname
+     //     const options = {
+     //        continueOnError: false
+     //     }
+     //     const uploadResponse = await artifactClient.uploadArtifact(artifactOverview, files_overview, rootDirectory, options)
+     // }else{
+     //     core.info('No overview files to build the artifact. ')
+     // }
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
 async function run() {
-    build_overview();
+    // build_overview();
     build_artifacts();
 }
 
