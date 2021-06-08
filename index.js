ebeb6e8612c0b11981125a19796eba6dfe7dacd0
mabdi <m.abdi@aut.ac.ir>
2021-06-08 12:51:04 +0200

    refactoring started

--- index.js
+++ index.js
@@ -1,43 +1,83 @@
+const child_process = require('child_process')
 const core = require('@actions/core');
 const io = require('@actions/io')
 const tc = require('@actions/tool-cache')
 const path = require('path')
 const os = require('os')
 
+const PHARO_ZEROCONF_URL = 'get.pharo.org/64/stable'
+const PHARO_HOME = path.join(os.homedir(), '.pharo')
+const PHARO_VM = 'vm'
+const PHARO_IMAGE = 'Pharo.image'
 
-const SMALLAMP_CI_HOME = path.join(os.homedir(), '.smallAmpCI')
-const SMALLAMP_TONEL = path.join(SMALLAMP_CI_HOME, 'tonel')
-const SMALLAMP_CI_ZIPS = path.join(SMALLAMP_CI_HOME, '_zip')
-const SMALLAMP_DOWNLOAD = 'https://github.com/mabdi/SmallAmp-runner/archive/master.tar.gz'
 const SMALLAMP_TONEL_DOWNLOAD = 'https://github.com/mabdi/small-amp/archive/master.tar.gz'
+const SMALLAMP_HOME = path.join(os.homedir(), '.smallAmp')
+const SMALLAMP_RUNNER = path.join(SMALLAMP_HOME, 'runner')
+const SMALLAMP_SCRIPTS = path.join(SMALLAMP_HOME, 'scripts')
+const SMALLAMP_ZIPS = path.join(SMALLAMP_HOME, '_zip')
 
-// most @actions toolkit packages have async methods
-async function run() {
-  try {
+const action = core.getInput('action', { required: true });
 
-     let tempDir = path.join(os.homedir(), '.smallAmpCI-temp')
-     const toolPath = await tc.downloadTool(SMALLAMP_DOWNLOAD)
-     tempDir = await tc.extractTar(toolPath, tempDir)
-     await io.mv(path.join(tempDir, 'SmallAmp-runner-master'), SMALLAMP_CI_HOME)
-
-     const tonelPath = await tc.downloadTool(SMALLAMP_TONEL_DOWNLOAD)
-     tempDir = await tc.extractTar(tonelPath, tempDir)
-     await io.mv(path.join(tempDir, 'small-amp-master'), SMALLAMP_TONEL)
-
-     await io.mkdirP(SMALLAMP_CI_ZIPS);
-     core.exportVariable('SMALLAMP_CI_ZIPS', SMALLAMP_CI_ZIPS);
-     core.exportVariable('SMALLAMP_TONEL', SMALLAMP_TONEL);
-
-     core.addPath(path.join(SMALLAMP_CI_HOME, 'bin'))
-     core.exportVariable('SMALLAMP_CI_HOME',SMALLAMP_CI_HOME);
-     core.exportVariable('SMALLAMP_BRANCH_NAME','SmallAmp-Run' + process.env.GITHUB_RUN_NUMBER);
-
-    
-     
-
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+async function download_Pharo(){
+  dest = 'pharoZeroConf'
+  const zeroConf = await tc.downloadTool(PHARO_ZEROCONF_URL, dest)
+  await io.mv(zeroConf, PHARO_HOME)
+  child_process.execSync('bash ' + dest, {cwd: PHARO_HOME})
 }
 
-run();
+async function download_SmallAmp(){
+  let tempDir = path.join(os.homedir(), '.smallAmp-temp')
+  const tonelPath = await tc.downloadTool(SMALLAMP_TONEL_DOWNLOAD)
+  tempDir = await tc.extractTar(tonelPath, tempDir)
+  await io.mv(path.join(tempDir, 'small-amp-master'), SMALLAMP_HOME)
+  await io.mkdirP(SMALLAMP_ZIPS);
+  core.exportVariable('SMALLAMP_HOME', SMALLAMP_HOME);
+  core.exportVariable('SMALLAMP_TONEL', SMALLAMP_HOME);
+  core.addPath(path.join(SMALLAMP_RUNNER, 'bin'))
+  core.exportVariable('SMALLTALK_CI_VM', path.join(PHARO_HOME, PHARO_VM));
+  core.exportVariable('SMALLTALK_CI_IMAGE', path.join(PHARO_HOME, PHARO_IMAGE));
+  core.exportVariable('SMALLAMP_CI_ZIPS', SMALLAMP_ZIPS);
+  
+}
+
+async function eval_Pharo(script){
+  cmd = PHARO_VM + ' ' + PHARO_IMAGE + ' eval "' + script + '"'
+  child_process.execSync(cmd, {cwd: PHARO_HOME})
+}
+
+async function eval_st_Pharo(scriptName){
+  cmd = PHARO_VM + ' ' + PHARO_IMAGE + ' st "' + scriptName + '"'
+  child_process.execSync(cmd, {cwd: PHARO_HOME})
+}
+
+async function run_st_script(scriptName){
+  await io.mv(path.join(SMALLAMP_SCRIPTS, scriptName), PHARO_HOME)
+  eval_st_Pharo(scriptName)
+}
+
+async function setup_run() {
+  download_Pharo()
+  download_SmallAmp()
+  run_st_script('load_project.st')
+  run_st_script('run_tests.st')
+  run_st_script('load_SmallAmp.st')
+}
+
+async function amplify_run() {
+  child_process.execSync(cmd, {cwd: PHARO_HOME})
+}
+
+async function push_run() {
+
+}
+
+try {
+  if(action == 'setup')
+    setup_run();
+  if(action == 'amplify')
+    amplify_run();
+  if(action == 'push')
+    push_run();
+} catch (error) {
+  core.setFailed(error.message);
+}  
