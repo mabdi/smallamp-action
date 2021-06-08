9faaed3f1ac3702f85d6c3d777ea8f2cc0d1e3bb
mabdi <m.abdi@aut.ac.ir>
2021-06-08 16:15:49 +0200

    dev

--- dist/index.js
+++ dist/index.js
@@ -36,6 +36,7 @@ const SMALLAMP_SCRIPTS = path.join(SMALLAMP_HOME, 'scripts')
 const SMALLAMP_ZIPS = path.join(SMALLAMP_HOME, '_zip')
 
 const action = core.getInput('action', { required: true });
+const REPO_NAME = process.env.reponame
 
 async function install_Pharo(){
   await io.mkdirP(PHARO_HOME);
@@ -43,7 +44,7 @@ async function install_Pharo(){
   await io.mv(path.join(SMALLAMP_SCRIPTS, 'installPharo.sh'), PHARO_HOME)
   child_process.execSync('./installPharo.sh', {cwd: PHARO_HOME})
   await logMe('After zeroconf ls PharoHome: \n'+ child_process.execSync('ls -al', {cwd: PHARO_HOME}))
-  let version = await eval_Pharo('Smalltalk version')
+  let version = await run_Pharo('eval "Smalltalk version"')
   await logMe('Pharo installed: version +', version)
   // core.exportVariable('SMALLTALK_CI_VM', path.join(PHARO_HOME, PHARO_VM));
   // core.exportVariable('SMALLTALK_CI_IMAGE', path.join(PHARO_HOME, PHARO_IMAGE));
@@ -60,32 +61,44 @@ async function download_SmallAmp(){
   await io.mkdirP(SMALLAMP_ZIPS);
   await logMe('ls SMALLAMP_HOME: \n'+ child_process.execSync('ls -al', {cwd: SMALLAMP_HOME}))
   // core.exportVariable('SMALLAMP_HOME', SMALLAMP_HOME);
-  // core.exportVariable('SMALLAMP_TONEL', SMALLAMP_HOME);
+  core.exportVariable('SMALLAMP_TONEL', SMALLAMP_HOME);
   // core.addPath(path.join(SMALLAMP_RUNNER, 'bin'))
 }
 
-async function eval_Pharo(script){
-  cmd = './' + PHARO_VM + ' ' + PHARO_IMAGE + ' eval "' + script + '"'
-  child_process.execSync(cmd, {cwd: PHARO_HOME})
-}
-
-async function eval_st_Pharo(scriptName){
-  cmd = './' + PHARO_VM + ' ' + PHARO_IMAGE + ' st "' + scriptName + '"'
+async function run_Pharo(arg){
+  cmd = './' + PHARO_VM + ' ' + PHARO_IMAGE + ' ' + arg
   child_process.execSync(cmd, {cwd: PHARO_HOME})
 }
 
 async function run_st_script(scriptName){
   await logMe('Running script ', scriptName)
   await io.mv(path.join(SMALLAMP_SCRIPTS, scriptName), PHARO_HOME)
-  await eval_st_Pharo(scriptName)
+  await run_Pharo('st ' + scriptName)
 }
 
 async function load_project(){
+  // needs 
   // project_baseline should be set
   // project_directory should be set
   // project_load if necessary
+  // GITHUB_WORKSPACE
+  // reponame
   core.exportVariable('project_repository', process.env.GITHUB_WORKSPACE + '/' + process.env.project_directory);
   await run_st_script('load_project.st')
+  await logMe('Project loading done')
+}
+
+async function load_SmallAmp(){
+  // needs 
+  // SMALLAMP_TONEL
+  await run_st_script('load_SmallAmp.st')
+  await logMe('SmallAmp loading done')
+}
+
+async function stat_project(){
+  await run_Pharo('smallamp  --save --stat=' + REPO_NAME)
+  await logMe('Stat eval done:')
+  child_process.execSync('cat '+ REPO_NAME + '.stat', {cwd: PHARO_HOME})
 }
 
 async function setup_run() {
@@ -95,10 +108,10 @@ async function setup_run() {
   await install_Pharo()
   await logMe('***************Load project')
   await load_project()
-  // await logMe('***************Run tests')
-  // await run_st_script('run_tests.st')
-  // await logMe('***************Load SmallAmp')
-  // await run_st_script('load_SmallAmp.st')
+  await logMe('***************Load SmallAmp')
+  await load_SmallAmp()
+  await logMe('***************Project stat')
+  await stat_project()
 }
 
 async function amplify_run() {
