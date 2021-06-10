ad132ac3405048ceb1eda905d165c4661e290874
mabdi <m.abdi@aut.ac.ir>
2021-06-10 11:04:03 +0200

    dev

--- dist/index.js
+++ dist/index.js
@@ -59,7 +59,7 @@ async function install_Pharo(){
   // let version = await run_Pharo("eval 'Smalltalk version'")
   
   core.exportVariable('PHARO_HOME', PHARO_HOME);
-  core.exportVariable('PHARO_VM', PHARO_VM);
+  core.exportVariable('PHARO_VM', path.join(PHARO_HOME, PHARO_VM));
   core.exportVariable('PHARO_IMAGE', PHARO_IMAGE);
   // core.exportVariable('SMALLTALK_CI_VM', path.join(PHARO_HOME, PHARO_VM));
   // core.exportVariable('SMALLTALK_CI_IMAGE', path.join(PHARO_HOME, PHARO_IMAGE));
