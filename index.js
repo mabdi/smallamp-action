a99a5177711831e485aed6e5fbe12e12a2940dff
Mehrdad Abdi <m.abdi@aut.ac.ir>
2022-01-25 12:22:46 +0100

    fix

--- index.js
+++ index.js
@@ -313,7 +313,9 @@ async function create_commit_from_amplified_classes(){
   await run_st_script('installer.st')
   // await logMe('Before commit ls d:\n' + child_process.execSync('ls -al', {cwd: cloneLocation}))
   await logMe('git status after:\n' + child_process.execSync("git status", {cwd: cloneLocation}))
-  const n_changed_files = child_process.execSync("git diff --name-only", {cwd: cloneLocation}).split(/\r\n|\r|\n/).length
+  var n_changed_files = '' + child_process.execSync("git diff --name-only", {cwd: cloneLocation})
+  await logMe('Files to commit:\n' + n_changed_files)
+  n_changed_files = n_changed_files.split(/\r\n|\r|\n/).length
   if(n_changed_files>0){
     await logMe('Lets push files. Number of changed files: ' + n_changed_files)
     child_process.execSync(`git config user.name ${COMMIT_USER}`, {cwd: cloneLocation})
